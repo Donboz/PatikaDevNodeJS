22c18fb3f706c72f9f3e5d454b3e23db9f9ed302
Murat Batak <muratbatak052005@gmail.com>
2026-07-21 04:26:31 +0300

    SQL 4.Ödev

---
+++ SQL/Ödev 4/ödev.sql
@@ -0,0 +1,13 @@
+
+SELECT DISTINCT replacement_cost FROM film;
+
+SELECT COUNT(DISTINCT replacement_cost) FROM film;
+
+SELECT COUNT(title) FROM film
+WHERE title LIKE 'T%' and rating = 'G';
+
+SELECT COUNT(country) FROM country
+WHERE LENGTH(country) = 5;
+
+SELECT COUNT(city) FROM city
+WHERE city ~~* '%R';
